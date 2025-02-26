7984c0b9803e208eddf4e25e70b142c56d7821dd
nsirtak <nsirtak@gmail.com>
2025-02-25 20:34:02 -0600

    fixes

--- server.js
+++ server.js
@@ -5,12 +5,53 @@ import express from 'express';
 const app = express();
 import morgan from 'morgan';
 import mongoose from 'mongoose';
+import cookieParser from 'cookie-parser';
+import cloudinary from 'cloudinary';
+import helmet from 'helmet';
+import mongoSanitize from 'express-mongo-sanitize';
 
 //routers
 import postRouter from './routers/postRouter.js';
 import authRouter from './routers/authRouter.js';
+import userRouter from './routes/userRouter.js';
+// public
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
 //middleware
 import errorHandlerMiddleware from './middleware/errorHandlerMiddleware.js';
+import { authenticateUser } from './middleware/authMiddleware.js';
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+app.use(express.static(path.resolve(__dirname, './client/dist')));
+app.use(cookieParser());
+app.use(express.json());
+app.use(helmet());
+app.use(mongoSanitize());
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+app.get('/api/v1/test', (req, res) => {
+  res.json({ msg: 'test route' });
+});
+app.use('/api/v1/jobs', authenticateUser, jobRouter);
+app.use('/api/v1/users', authenticateUser, userRouter);
+app.use('/api/v1/auth', authRouter);
+
+
+
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
@@ -19,9 +60,7 @@ if (process.env.NODE_ENV === 'development') {
 app.use(morgan('dev')); // log requests to the console
 
 app.use(express.json());
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+
 
 app.post('/', (req, res) => {
   console.log(req);
@@ -31,19 +70,22 @@ app.post('/', (req, res) => {
 app.use('/api/v1/posts', postRouter);
 app.use('/api/v1/auth', authRouter);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './client/dist', 'index.html'));
+});
 //Not Found Middleware
 app.use('*', (req, res) => {
   res.status(404).json({ msg: 'not found' });
 });
-
 //Error Middleware
 app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5100;
+
 try {
   await mongoose.connect(process.env.MONGO_URL);
   app.listen(port, () => {
-    console.log(`server running on PORT ${port}....`);
+    console.log(`server running on PORT ${port}...`);
   });
 } catch (error) {
   console.log(error);
